0757392d748d43954c5dd3b1ea280956b7d53458
Diao Diallo <diao_diallo@gn.jsi.com>
2018-09-26 00:07:33 +0000

    Attach to fpntc Theme

--- js/one-select.js
+++ js/one-select.js
@@ -1,48 +1,66 @@
+(function ($, Drupal, Bootstrap){
 
 
-(function ($){
 
 
+    jQuery(document).ready(function() {
 
-  var limitSelection = function () {
+        console.log('Theme : In the beginning of the JS file');
 
+        //jQuery(".dropdown-toggle").clone().appendTo("#myCheckboxes");
 
-  $("input[type=checkbox]").on("click", function () {
+        $(".form-checkbox").on("click", function () {
 
-    if ($(this).prop('checked')) {
+            console.log('Checkbox checked!');
 
-      $("input[type=checkbox]").each(function () {
-        if ($(this).prop('checked')) {
+        });
 
-        }
-        else {
-          $(this).prop('disabled', true);
-        }
-      });
+        /*$("input[type=checkbox]").on("click", function () {
 
-    }
-    else {
+          console.log('Checkbox checked!');
 
-      $("input[type=checkbox]").each(function () {
+        });*/
 
-        $(this).prop('disabled', false);
-
-      });
-
-    }
-  });
-  };
-
-  limitSelection();
+        var limitSelection = function () {
 
 
-   //When the user paged the table @Class : parent of the table
-  $(".view-training-list-item-browser-search-api").on('change', function () {
+            $("input[type=checkbox]").on("click", function () {
 
-    console.log('Paged!');
-    limitSelection();
+                if ($(this).prop('checked')) {
 
-  });
+                    $("input[type=checkbox]").each(function () {
+                        if ($(this).prop('checked')) {
 
-})(jQuery);
+                        }
+                        else {
+                            $(this).prop('disabled', true);
+                        }
+                    });
 
+                }
+                else {
+
+                    $("input[type=checkbox]").each(function () {
+
+                        $(this).prop('disabled', false);
+
+                    });
+
+                }
+            });
+        };
+
+        limitSelection();
+
+        //When the user paged the table @Class : parent of the table
+        $(".view-training-list-item-browser-search-api").on('change', function () {
+
+            console.log('Paged!');
+            limitSelection();
+
+        });
+
+    });
+
+
+})(window.jQuery, window.Drupal, window.Drupal.bootstrap);
